c81549c824ebc3069381d411081a59ee060d2277
Kremenchuk Igor <makiintoch@gmail.com>
2018-12-28 12:07:12 +0200

    update tests and xml

--- index.php
+++ index.php
@@ -5,19 +5,19 @@ require_once('./vendor/autoload.php');
 use App\Loader\Loader;
 use App\Parser\{ParseGroups, ParseProducts, ParseWarehouses};
 
-$products_url = './storage/xml/products.xml';
-$groups_url = './storage/xml/import.xml';
+$import_url = './storage/xml/import.xml';
+$classifier_url = './storage/xml/classifier.xml';
 
 $loader = new Loader();
 $productParser = new ParseProducts();
 $groupParser = new ParseGroups();
 $warehouseParser = new ParseWarehouses();
 
-$product_xml = $loader->load($products_url);
-$import_xml = $loader->load($groups_url);
+$import_xml = $loader->load($import_url);
+$classifier_xml = $loader->load($classifier_url);
 
-$products = $productParser->getItems($product_xml);
-$groups = $groupParser->getItems($import_xml);
-$warehouses = $warehouseParser->getItems($import_xml);
+$products = $productParser->getItems($import_xml);
+$groups = $groupParser->getItems($classifier_xml);
+$warehouses = $warehouseParser->getItems($classifier_xml);
 
 dd($warehouses);
--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -10,15 +10,15 @@ class ParserTest extends \PHPUnit\Framework\TestCase
     public function testCategoriesCount(): void
     {
         $parser = new ParseGroups();
-        $xml = $this->loadXml('./storage/xml/import.xml');
+        $xml = $this->loadXml('./storage/xml/classifier.xml');
         $groups = $parser->getItems($xml);
-        $this->assertEquals(33, count($groups));
+        $this->assertEquals(2, count($groups));
     }
 
     public function testProductsCount(): void
     {
         $parser = new ParseProducts();
-        $xml = $this->loadXml('./storage/xml/products.xml');
+        $xml = $this->loadXml('./storage/xml/import.xml');
         $products = $parser->getItems($xml);
         $this->assertEquals(11, count($products));
     }
@@ -26,9 +26,9 @@ class ParserTest extends \PHPUnit\Framework\TestCase
     public function testWarehousesCount(): void
     {
         $parser = new ParseWarehouses();
-        $xml = $this->loadXml('./storage/xml/import.xml');
+        $xml = $this->loadXml('./storage/xml/classifier.xml');
         $warehouses = $parser->getItems($xml);
-        $this->assertEquals(6, count($warehouses));
+        $this->assertEquals(14, count($warehouses));
     }
 
     private function loadXml(string $xml)
